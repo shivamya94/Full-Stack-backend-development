ded36ae4a3d44578c6a0d051567c005779c30c64
Shivamya94 <yadavshivam90440@gmail.com>
2024-12-28 23:25:39 +0530

    file added

--- fileUpload/controllers/fileUpload.js
+++ fileUpload/controllers/fileUpload.js
@@ -4,17 +4,21 @@ const File = require("../models/File");
 
 exports.localFileUpload = async (req,res) => {
     try {
-        //fetch file
+        //fetch file from request
         const file = req.files.file;
         console.log("File Agayi jee ->", file);
+        
 
+        //create path where file need to be stored on server file
         let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`;
         console.log("Path ->", path)
-
+        
+        //add path to the move function
         file.mv(path , (err) => {
             console.log(err);
         });
-
+        
+        //create a successful response
         res.json({
             success:true,
             message:'Local file uploaded Successfully',
