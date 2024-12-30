63719bf18cf820bc0ae42bc25397f169deb4a530
Shivamya94 <yadavshivam90440@gmail.com>
2024-12-31 02:28:39 +0530

    file added

--- fileUpload/controllers/fileUpload.js
+++ fileUpload/controllers/fileUpload.js
@@ -40,6 +40,7 @@ function isFileTypeSupported(type, supportedTypes) {
 async function uploadFileToCloudinary(file, folder) {
     const options = {folder};
     console.log("temp file path ", file.tempFilePath);
+    options.resource_type = "auto";
     return  await cloudinary.uploader.upload(file.tempFilePath, options);
     
 }
